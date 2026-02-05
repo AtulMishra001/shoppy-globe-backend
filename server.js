303b93f300031098bb7837ac3060813d7e3e4c40
AtulMishra001 <atulm7151@gmail.com>
2026-02-05 18:39:29 +0530

    mongodb connection is created

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./src/config/db.js";
 
 // Load environment variables
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
