c1efcd96975b2374a7b485e20d363fd5127c9a2f
AtulMishra001 <atulm7151@gmail.com>
2026-02-05 18:24:08 +0530

    Init: Setup Node.js project with ES Modules and Express server

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json()); // Allow JSON data in requests
+app.use(cors());
+
+// Simple Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
